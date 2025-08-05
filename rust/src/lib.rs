a67f6afa4cffd13b3da1a2d1e916f44af5388749
Isaac Mills <rooster0055@protonmail.com>
2025-08-05 15:11:27 -0600

    Implement boilerplate for Terminal node

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,11 @@
-use godot::{
-    classes::{ISprite2D, Sprite2D},
-    prelude::*,
-};
-use tracing::instrument;
+use godot::prelude::*;
 use tracing_error::ErrorLayer;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
+mod terminal;
 mod tracing_godot;
+
+pub use terminal::Terminal;
 
 struct FnafDoubleVisionExtension;
 
@@ -33,49 +32,5 @@ unsafe impl ExtensionLibrary for FnafDoubleVisionExtension {
                 )
                 .init();
         }
-    }
-}
-
-#[derive(GodotClass)]
-#[class(base=Sprite2D)]
-struct Player {
-    speed: f64,
-    angular_speed: f64,
-
-    base: Base<Sprite2D>,
-}
-
-#[godot_api]
-impl ISprite2D for Player {
-    #[instrument(skip_all)]
-    fn init(base: Base<Sprite2D>) -> Self {
-        tracing::info!("Hello, world!"); // Prints to the Godot console
-
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
-    }
-
-    fn physics_process(&mut self, delta: f64) {
-        // GDScript code:
-        //
-        // rotation += angular_speed * delta
-        // var velocity = Vector2.UP.rotated(rotation) * speed
-        // position += velocity * delta
-
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
-
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        self.base_mut().translate(velocity * delta as f32);
-
-        // or verbose:
-        // let this = self.base_mut();
-        // this.set_position(
-        //     this.position() + velocity * delta as f32
-        // );
     }
 }
