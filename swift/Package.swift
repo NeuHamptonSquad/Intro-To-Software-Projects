43d7f05225cca58636f2a16cebbff9dc9f5966dc
Isaac Mills <rooster0055@protonmail.com>
2025-08-09 17:12:20 -0600

    Hopefully this makes hot reload work

--- swift/Package.swift
+++ swift/Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "fnaf",
-    platforms: [.macOS(.v14)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "fnaf",
-            type: .dynamic,
-            targets: ["fnaf"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "fnaf",
-            dependencies: [
-                "SwiftGodot",
-            ],
-            swiftSettings: [.unsafeFlags(["-suppress-warnings"])]
-        ),
+  name: "fnaf",
+  platforms: [.macOS(.v14)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "fnaf",
+      type: .dynamic,
+      targets: ["fnaf"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/StratusFearMe21/SwiftGodot", branch: "main")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "fnaf",
+      dependencies: [
+        "SwiftGodot"
+      ],
+      swiftSettings: [.unsafeFlags(["-suppress-warnings"])]
+    )
 
-    ]
+  ]
 )
